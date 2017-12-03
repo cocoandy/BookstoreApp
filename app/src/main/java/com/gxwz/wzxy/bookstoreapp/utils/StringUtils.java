36dfb63e6f5d078b54a7feca28e32bf0aa094780
crucy <wx_bin@sina.com>
2017-12-04 00:03:55 +0800

    类型

---
+++ app/src/main/java/com/gxwz/wzxy/bookstoreapp/utils/StringUtils.java
@@ -0,0 +1,28 @@
+package com.gxwz.wzxy.bookstoreapp.utils;
+
+/**
+ * Created by crucy on 2017/12/3.
+ */
+
+public class StringUtils {
+    /**
+     * 获取操作状态
+     * @param type
+     * @return
+     */
+    public String getContrlType(int type) {
+        switch (type) {
+            case 0:
+                return "未付款";
+            case 1:
+                return "待收货";
+            case 2:
+                return "待评论";
+            case 3:
+                return "退款中";
+            case 4:
+                return "完成";
+        }
+        return "处理中";
+    }
+}
